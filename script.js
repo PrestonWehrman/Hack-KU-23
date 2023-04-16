873e5d10d172e4346cbfe51749e91ae1746bf923
gustavoMoreira1 <g524m889@ku.edu>
2023-04-15 21:42:51 -0500

    mapping not quite finished but here...

--- script.js
+++ script.js
@@ -4,20 +4,6 @@ var nt = now.toISOString().slice(0, 16);
 console.log(nt);
 document.getElementById("time").setAttribute("value", nt);
 
-document
-    .getElementById('floor')
-    .addEventListener('change', function () {
-        'use strict';
-        var vis = document.querySelector('.vis'),
-            target = document.getElementById(this.value);
-        if (vis !== null) {
-            vis.className = 'inv';
-        }
-        if (target !== null) {
-            target.className = 'vis';
-        }
-    });
-
     // Get the modal
     var modal = document.getElementById("myModal");
     // Get the button that opens the modal
